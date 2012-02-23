febe7bcee90410568d0b79fb148edec88c7cdc2b
Bryan Catanzaro <bcatanzaro@acm.org>
2012-02-22 17:10:50 -0800

    New data structures working in Python runtime. Compiler not yet updated.

---
+++ src/copperhead/runtime/cudata.cpp
@@ -0,0 +1,167 @@
+#include <boost/python.hpp>
+#include "cuarray.h"
+#include "make_cuarray.h"
+#include "make_cuarray_impl.h"
+#include "make_sequence.h"
+#include "make_sequence_impl.h"
+#include "cunp.h"
+#include "type.hpp"
+#include "monotype.hpp"
+#include "type_printer.hpp"
+#include <sstream>
+
+using std::shared_ptr;
+using std::make_shared;
+using std::ostringstream;
+using std::string;
+
+typedef boost::shared_ptr<cuarray> sp_cuarray;
+
+template<typename T>
+sp_cuarray make_cuarray_copy(T* d, ssize_t n) {
+    sp_cuarray new_array = make_cuarray<T>(n);
+    sequence<T> new_seq = make_sequence<sequence<T> >(new_array, true, true);
+    memcpy(new_seq.m_d, d, sizeof(T) * n);
+    return new_array;
+}
+
+sp_cuarray make_cuarray_PyObject(PyObject* in) {
+    np_array_info in_props = inspect_array(in);
+    shared_ptr<backend::sequence_t> t =
+        std::static_pointer_cast<backend::sequence_t>(std::get<2>(in_props));
+    if (t->p_sub() == backend::bool_mt) {
+        return make_cuarray_copy((bool*)std::get<0>(in_props),
+                                 std::get<1>(in_props));
+    } else if (t->p_sub() == backend::int32_mt) {
+        return make_cuarray_copy((int*)std::get<0>(in_props),
+                                 std::get<1>(in_props));
+    } else if (t->p_sub() == backend::int64_mt) {
+        return make_cuarray_copy((long*)std::get<0>(in_props),
+                                 std::get<1>(in_props));
+    } else if (t->p_sub() == backend::float32_mt) {
+        return make_cuarray_copy((float*)std::get<0>(in_props),
+                                 std::get<1>(in_props));
+    } else if (t->p_sub() == backend::float64_mt) {
+        return make_cuarray_copy((double*)std::get<0>(in_props),
+                                 std::get<1>(in_props));
+    } else {
+        throw std::invalid_argument("Can't create CuArray from this object");
+    }
+}
+
+
+std::shared_ptr<backend::type_t> type_derive(const cuarray& in) {
+    return in.m_t;
+}
+
+
+
+class cuarray_iterator {
+private:
+    sp_cuarray source;
+    ssize_t index;
+public:
+    cuarray_iterator(sp_cuarray& _source) : source(_source), index(0) {}
+    PyObject* next() {
+        if (index >= ssize_t(source->m_l[0])) {
+            PyErr_SetString(PyExc_StopIteration, "No more data.");
+            boost::python::throw_error_already_set();
+        }
+        std::shared_ptr<backend::sequence_t> seq_t =
+            std::static_pointer_cast<backend::sequence_t>(source->m_t);
+        std::shared_ptr<backend::type_t> sub_t = seq_t->p_sub();
+        if (sub_t == backend::int32_mt) {
+            sequence<int> s = make_sequence<sequence<int> >(source, true, false);
+            int result = s[index++];
+            return make_scalar(result);
+        } else if (sub_t == backend::int64_mt) {
+            sequence<long> s = make_sequence<sequence<long> >(source, true, false);
+            long result = s[index++];
+            return make_scalar(result);
+        } else if (sub_t == backend::float32_mt) {
+            sequence<float> s = make_sequence<sequence<float> >(source, true, false);
+            float result = s[index++];
+            return make_scalar(result);
+        } else if (sub_t == backend::float64_mt) {
+            sequence<double> s = make_sequence<sequence<double> >(source, true, false);
+            double result = s[index++];
+            return make_scalar(result);
+        } else if (sub_t == backend::bool_mt) {
+            sequence<bool> s = make_sequence<sequence<bool> >(source, true, false);
+            bool result = s[index++];
+            return make_scalar(result);
+        }
+        throw std::invalid_argument("Can't print arrays of this type");
+    }
+};
+
+shared_ptr<cuarray_iterator>
+make_iterator(sp_cuarray& in) {
+    return make_shared<cuarray_iterator>(in);
+}
+
+
+template<class T>
+T* get_pointer(shared_ptr<T> const &p) {
+    return p.get();
+}
+
+
+void print_array(sp_cuarray& in, ostringstream& os) {
+    std::shared_ptr<backend::sequence_t> seq_t = std::static_pointer_cast<backend::sequence_t>(in->m_t);
+    std::shared_ptr<backend::type_t> sub_t = seq_t->p_sub();
+    if (sub_t == backend::int32_mt) {
+        sequence<int> s = make_sequence<sequence<int> >(in, true, false);
+        os << s;
+    } else if (sub_t == backend::int64_mt) {
+        sequence<long> s = make_sequence<sequence<long> >(in, true, false);
+        os << s;
+    } else if (sub_t == backend::float32_mt) {
+        sequence<float> s = make_sequence<sequence<float> >(in, true, false);
+        os << s;
+    } else if (sub_t == backend::float64_mt) {
+        sequence<double> s = make_sequence<sequence<double> >(in, true, false);
+        os << s;
+    } else if (sub_t == backend::bool_mt) {
+        sequence<bool> s = make_sequence<sequence<bool> >(in, true, false);
+        os << s;
+    }
+}
+
+void print_type(sp_cuarray&in, ostringstream& os) {
+    backend::repr_type_printer tp(os);
+    boost::apply_visitor(tp, *(in->m_t));
+}
+
+string repr_cuarray(sp_cuarray& in) {
+    ostringstream os;
+    os << "cuarray(";
+    print_array(in, os);
+    os << ", type=";
+    print_type(in, os);
+    os << ")";
+    return os.str();
+}
+
+string str_cuarray(sp_cuarray& in) {
+    ostringstream os;
+    print_array(in, os);
+    return os.str();
+}
+
+BOOST_PYTHON_MODULE(cudata) {
+    using namespace boost::python;
+    
+    class_<cuarray, boost::shared_ptr<cuarray>, boost::noncopyable >("cuarray", no_init)
+        .def("__init__", make_constructor(make_cuarray_PyObject))
+        .def("__repr__", repr_cuarray)
+        .def("__str__", str_cuarray)
+        .add_property("type", type_derive)
+    //     .def("np", np)
+        .def("__iter__", make_iterator);
+    class_<cuarray_iterator, shared_ptr<cuarray_iterator> >
+        ("cuarrayiterator", no_init)
+        .def("next", &cuarray_iterator::next)
+        ;
+    
+}
---
+++ tests/test_sort.py
@@ -0,0 +1,49 @@
+#
+#   Copyright 2012      NVIDIA Corporation
+# 
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+# 
+#       http://www.apache.org/licenses/LICENSE-2.0
+# 
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+# 
+#
+from copperhead import *
+import numpy as np
+import unittest
+import random
+
+@cu
+def lt_sort(x):
+    return sort(cmp_lt, x)
+
+@cu
+def gt_sort(x):
+    return sort(cmp_gt, x)
+
+class SortTest(unittest.TestCase):
+    def setUp(self):
+        self.source = np.array([random.random() for x in range(5)], dtype=np.float32)
+        
+        
+    def run_test(self, fn, *args):
+        cpuResult = fn(*args, target_place=places.here)
+        gpuResult = fn(*args, target_place=places.gpu0)
+        self.assertEqual(list(cpuResult), list(gpuResult))
+    
+    def testLtSort(self):
+        self.run_test(lt_sort, self.source)
+
+    def testGtSort(self):
+        self.run_test(gt_sort, self.source)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
